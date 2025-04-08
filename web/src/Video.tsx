3c97aeee36e6d23d5569622c1af6f2537642a985
Yu Daike <yu.daike@suse.com>
2025-04-09 00:20:19 +0200

    feat: gift display in chat

--- web/src/Video.tsx
+++ web/src/Video.tsx
@@ -34,14 +34,44 @@ const VODInfo: FC<{ info: VideoInfo | null }> = ({ info }) => {
     )
 }
 
+interface ChatGift {
+    id: number
+    type: "gift"
+    timestamp: number
+    username: string
+    gift_name: string
+    count: number
+}
+
+interface ChatSub {
+    id: number
+    type: "sub"
+    timestamp: number
+    username: string
+    sub_name: string
+    count: number
+}
+
+interface ChatSC {
+    id: number
+    type: "sc"
+    timestamp: number
+    username: string
+    content: string
+    price: number
+}
+
 interface ChatMsg {
     id: number,
+    type: "message"
     timestamp: number,
     username: string,
     content: string
 }
 
-const ChatEntry: FC<{ msg: ChatMsg }> = ({ msg }) => {
+type ChatEntry = ChatGift | ChatSub | ChatMsg | ChatSC
+
+const ChatMsgEntry: FC<{ msg: ChatMsg }> = ({ msg }) => {
     return (
         <Group gap="1">
             <Text fw={700}>{msg.username}:</Text>
@@ -50,18 +80,74 @@ const ChatEntry: FC<{ msg: ChatMsg }> = ({ msg }) => {
     )
 }
 
-// Binary search to find the newest message until current playback position
-// `msgs` must be sorted
-function binary_search_msg(msgs: ChatMsg[], playback_position: number) {
+const ChatGiftEntry: FC<{ gift: ChatGift }> = ({ gift }) => {
+    return (
+        <Card shadow="xs" withBorder p="sm" bg="orange">
+            <Group gap="sm" justify="center">
+                <Text fw={700}>{gift.username}</Text>
+                <Text>赠送</Text>
+                <Text fw={700}>{gift.gift_name}</Text>
+                {gift.count > 1 && <Text>×{gift.count}</Text>}
+            </Group>
+        </Card>
+    )
+}
+
+const ChatSubEntry: FC<{ sub: ChatSub }> = ({ sub }) => {
+    return (
+        <Card shadow="xs" withBorder p="sm" bg="blue">
+            <Group gap="sm" justify="center">
+                <Text fw={700}>{sub.username}</Text>
+                <Text>续费</Text>
+                <Text fw={700}>{sub.sub_name}</Text>
+                {sub.count > 1 && <Text>×{sub.count}</Text>}
+            </Group>
+        </Card>
+    )
+}
+
+const ChatSCEntry: FC<{ sc: ChatSC }> = ({ sc }) => {
+    return (
+        <Card shadow="xs" withBorder p="xl" bg="red">
+            <Card.Section>
+                <Group gap="sm">
+                    <Text>¥{sc.price / 1000}</Text>
+                    <Text fw={700}>{sc.username}</Text>
+                </Group>
+            </Card.Section>
+
+            <Card.Section>
+                <Text>{sc.content}</Text>
+            </Card.Section>
+        </Card>
+    )
+}
+
+const ChatEntry: FC<{ entry: ChatEntry }> = ({ entry }) => {
+    switch (entry.type) {
+        case "message":
+            return <ChatMsgEntry msg={entry} />
+        case "gift":
+            return <ChatGiftEntry gift={entry} />
+        case "sub":
+            return <ChatSubEntry sub={entry} />
+        case "sc":
+            return <ChatSCEntry sc={entry} />
+    }
+}
+
+// Binary search to find the newest entries until current playback position
+// `entries` must be sorted
+function binary_search_entries(entries: ChatEntry[], playback_position: number) {
     let lower = 0
-    let upper = msgs.length
+    let upper = entries.length
     while (true) {
         let middle = Math.floor((upper - lower) / 2 + lower)
         if (middle == lower || middle == upper) {
             return upper
         }
 
-        if (msgs[middle].timestamp <= playback_position) {
+        if (entries[middle].timestamp <= playback_position) {
             lower = middle
         } else {
             upper = middle
@@ -70,9 +156,9 @@ function binary_search_msg(msgs: ChatMsg[], playback_position: number) {
 }
 
 const ChatDisplay: FC<{
-    messages: ChatMsg[]
+    entries: ChatEntry[]
     playbackPosition: number
-}> = ({ messages, playbackPosition }) => {
+}> = ({ entries, playbackPosition }) => {
     const following = true	// TODO
 
     const viewport = useRef<HTMLDivElement>(null)
@@ -86,18 +172,18 @@ const ChatDisplay: FC<{
         }
     }, [following])
 
-    const sorted_messages = useMemo(() => {
-        return messages.sort((a, b) => a.timestamp - b.timestamp)
-    }, [messages])
+    const sorted_entries = useMemo(() => {
+        return entries.sort((a, b) => a.timestamp - b.timestamp)
+    }, [entries])
 
     const [latest_idx, display_messages] = useMemo(() => {
         if (!following) {
-            return [0, sorted_messages]
+            return [0, sorted_entries]
         }
 
-        const latest_idx = binary_search_msg(sorted_messages, playbackPosition)
-        return [latest_idx, messages.slice(Math.max(latest_idx - 100, 0), latest_idx)]
-    }, [sorted_messages, following, playbackPosition])
+        const latest_idx = binary_search_entries(sorted_entries, playbackPosition)
+        return [latest_idx, entries.slice(Math.max(latest_idx - 100, 0), latest_idx)]
+    }, [sorted_entries, following, playbackPosition])
 
     useEffect(() => {
         if (!following) return
@@ -111,18 +197,49 @@ const ChatDisplay: FC<{
         <ScrollArea style={{ flex: "1 1 0", overflowY: "auto" }} viewportRef={viewport}>
             <Flex direction="column" gap="md">
                 {display_messages.map(msg => (
-                    <ChatEntry key={msg.id} msg={msg} />
+                    <ChatEntry key={msg.id} entry={msg} />
                 ))}
             </Flex>
         </ScrollArea>
     )
 }
 
+function parseDTag(item: Element, id: number): ChatMsg {
+    const timestamp = parseInt(item.getAttribute("p")!.split(",")[0], 10)
+    const username = item!.getAttribute("user")!
+    const content = item!.textContent!
+    return { id, type: "message", timestamp, username, content }
+}
+
+function parseToastTag(item: Element, id: number): ChatSub {
+    const timestamp = parseInt(item.getAttribute("ts")!, 10)
+    const username = item.getAttribute("user")!
+    const sub_name = item.getAttribute("role")!
+    const count = parseInt(item.getAttribute("count")!, 10)
+    return { id, type: "sub", timestamp, username, sub_name, count }
+}
+
+function parseGiftTag(item: Element, id: number): ChatGift {
+    const timestamp = parseInt(item.getAttribute("ts")!, 10)
+    const username = item.getAttribute("user")!
+    const gift_name = item.getAttribute("giftname")!
+    const count = parseInt(item.getAttribute("count")!, 10)
+    return { id, type: "gift", timestamp, username, gift_name, count }
+}
+
+function parseSCTag(item: Element, id: number): ChatSC {
+    const timestamp = parseInt(item.getAttribute("ts")!, 10)
+    const username = item.getAttribute("user")!
+    const content = item.textContent!;
+    const price = parseInt(item.getAttribute("price")!, 10)
+    return { id, type: "sc", timestamp, username, content, price }
+}
+
 const SideInfo: FC<{
     info: VideoInfo | null
     playbackPosition: number
 }> = ({ info, playbackPosition }) => {
-    const [messages, setMessages] = useState<ChatMsg[]>([])
+    const [entries, setEntries] = useState<ChatEntry[]>([])
 
     useEffect(() => {
         const loader = async () => {
@@ -136,17 +253,30 @@ const SideInfo: FC<{
             const xml = await res.text()
             const parser = new DOMParser()
             const doc = parser.parseFromString(xml, "text/xml")
+            const root = doc.firstElementChild!
 
-            const msgs: ChatMsg[] = []
-            const tags = doc.getElementsByTagName("d")
+            const entries: ChatEntry[] = []
+            const tags = root.children
             for (let i = 0; i < tags.length; ++i) {
-                const item = tags.item(i)
-                const timestamp = parseInt(item!.getAttribute("p")!.split(",")[0], 10)
-                const username = item!.getAttribute("user")!
-                const content = item!.textContent!
-                msgs.push({ id: i, timestamp, username, content })
+                const item = tags.item(i)!
+                switch (item.tagName) {
+                    case "d":
+                        entries.push(parseDTag(item, i))
+                        break
+                    case "toast":
+                        entries.push(parseToastTag(item, i))
+                        break
+                    case "gift":
+                        entries.push(parseGiftTag(item, i))
+                        break
+                    case "sc":
+                        entries.push(parseSCTag(item, i))
+                        break
+                    default:
+                        break
+                }
             }
-            setMessages(msgs)
+            setEntries(entries)
         }
         loader()
     }, [info])
@@ -154,7 +284,7 @@ const SideInfo: FC<{
     return (
         <Stack flex={{ base: 1, sm: 0 }} miw={{ sm: "300px" }} p="xl">
             <VODInfo info={info} />
-            <ChatDisplay playbackPosition={playbackPosition} messages={messages} />
+            <ChatDisplay playbackPosition={playbackPosition} entries={entries} />
         </Stack>
     )
 }

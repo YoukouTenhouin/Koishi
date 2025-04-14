edd8b594d4034bfb91a00066950b90c003f6f887
Youkou Tenhouin <youkou@tenhou.in>
2025-04-14 15:55:49 +0200

    refactor: clean up unused struct

--- cli/src/cmd/video/mod.rs
+++ cli/src/cmd/video/mod.rs
@@ -1,7 +1,4 @@
-use chrono::{DateTime, Utc};
 use clap::{Parser, Subcommand};
-use serde::{Deserialize, Serialize};
-use tabled::{Tabled, derive::display};
 
 mod create;
 mod import_from_xml;
@@ -14,23 +11,6 @@ mod upload;
 pub(crate) struct Args {
     #[command(subcommand)]
     command: Option<Commands>,
-}
-
-#[derive(Serialize, Deserialize, Tabled)]
-struct VideoInfoWithoutID {
-    #[tabled(rename = "Title")]
-    title: String,
-    #[tabled(rename = "Cover URL", display("display::option", ""))]
-    cover: Option<String>,
-    #[tabled(rename = "Room ID")]
-    room: u64,
-    #[tabled(rename = "Date", display("display_timestamp", self))]
-    timestamp: i64,
-}
-
-fn display_timestamp<T>(ts: &i64, _rec: &T) -> String {
-    let date: DateTime<Utc> = DateTime::from_timestamp_millis(ts.to_owned()).unwrap();
-    date.to_string()
 }
 
 #[derive(Subcommand)]

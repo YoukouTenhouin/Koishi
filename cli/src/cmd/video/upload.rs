7ed952743219e2aa9b9b16d627f058d9470fdd08
Youkou Tenhouin <youkou@tenhou.in>
2025-04-14 20:26:22 +0200

    feat: [cli] implement multi-threaded uploading

--- cli/src/cmd/video/upload.rs
+++ cli/src/cmd/video/upload.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressDrawTarget, ProgressStyle};
+use rayon::prelude::*;
 use std::{
     cmp::min,
     fs::File,
@@ -17,13 +18,15 @@ use crate::{api, helpers::cryptography::restricted_hash};
 #[derive(Parser)]
 pub(super) struct Args {
     #[arg(short = 'P', long)]
-    progress: bool,
-    #[arg(short = 's', long, default_value_t = 100_000_000)]
+    no_progress: bool,
+    #[arg(short = 's', long, default_value_t = 10_000_000)]
     part_size: u64,
     #[arg(short, long, default_value_t = 10)]
     retry_part: u64,
     #[arg(short, long)]
     password: Option<String>,
+    #[arg(short, long)]
+    thread_count: Option<usize>,
 
     uuid: String,
     path: PathBuf,
@@ -37,8 +40,13 @@ fn print_video_info(i: &api::video::Video) {
     println!("{table}");
 }
 
+struct UploadMultiProgress {
+    mp: MultiProgress,
+    pb_parts: ProgressBar,
+    pb_total: ProgressBar,
+}
+
 struct UploadProgress {
-    #[allow(dead_code)]
     mp: MultiProgress,
     pb_parts: ProgressBar,
     pb_total: ProgressBar,
@@ -46,77 +54,98 @@ struct UploadProgress {
 }
 
 struct UploadProgressReadWrapper<R: Read> {
-    inner: R,
-    pb_current: ProgressBar,
+    read: R,
     pb_total: ProgressBar,
+    pb_current: ProgressBar,
 }
 
-impl UploadProgress {
+impl UploadMultiProgress {
     fn new(parts: u64, total: u64) -> Self {
         let mp = MultiProgress::new();
 
-        let pb_parts = mp.add(ProgressBar::new(parts));
-        pb_parts.set_style(
-            ProgressStyle::default_bar()
-                .template("Parts   {bar:40.cyan/blue} {pos}/{len}")
-                .unwrap()
-                .progress_chars("##-"),
-        );
+        let pb_parts = mp.add(ProgressBar::new(parts))
+            .with_prefix("Parts")
+            .with_style(
+		ProgressStyle::default_bar()
+                    .template("{prefix:8} {wide_bar:40.cyan/blue} {pos}/{len}")
+                    .unwrap()
+                    .progress_chars("##-"),
+            );
         pb_parts.set_position(0);
 
-        let pb_total = mp.add(ProgressBar::new(total));
-        pb_total.set_style(
-            ProgressStyle::default_bar()
-                .template("Total   {bar:40.green/black} {bytes}/{total_bytes}")
-                .unwrap(),
-        );
+        let pb_total = mp.add(ProgressBar::new(total))
+            .with_prefix("Total")
+            .with_style(
+		ProgressStyle::default_bar()
+                    .template("{prefix:8} {wide_bar:40.green/black} {bytes}/{total_bytes} \
+			       {bytes_per_sec} elapsed {elapsed} ETA {eta}")
+                    .unwrap(),
+            );
 
-        let pb_current = mp.add(ProgressBar::no_length());
-        pb_current.set_style(
-            ProgressStyle::default_bar()
-                .template("Current {bar:40.yellow/black} {bytes}/{total_bytes}")
-                .unwrap(),
-        );
-
-        Self {
-            mp,
-            pb_parts,
-            pb_total,
-            pb_current,
-        }
+        Self { mp, pb_parts, pb_total }
     }
 
-    fn start_part<R: Read>(&self, read: R, part_size: u64) -> UploadProgressReadWrapper<R> {
-        self.pb_current.reset();
-        self.pb_current.set_length(part_size);
+    fn new_part(&self, part_idx: usize, part_size: u64) -> UploadProgress {
+        let pb_current = self.mp.add(ProgressBar::new(part_size))
+            .with_prefix(format!("#{}", part_idx+1))
+            .with_style(
+		ProgressStyle::default_bar()
+                    .template("{prefix:8} {wide_bar:40.yellow/black} {bytes}/{total_bytes} \
+			       {bytes_per_sec}")
+                .unwrap(),
+            )
+            .with_position(0);
 
-        UploadProgressReadWrapper::<R> {
-            inner: read,
+        UploadProgress {
+	    mp: self.mp.clone(),
+	    pb_parts: self.pb_parts.clone(),
             pb_total: self.pb_total.clone(),
-            pb_current: self.pb_current.clone(),
+	    pb_current
         }
     }
 
-    fn rollback_part(&self) {
-        let rollback_length = self.pb_current.position();
-        self.pb_current.dec(rollback_length);
-        self.pb_total.dec(rollback_length);
+    fn hide(&self) {
+	self.mp.set_draw_target(ProgressDrawTarget::hidden())
     }
 
-    fn finish_part(&self) {
-        self.pb_parts.inc(1);
+    fn println<S: AsRef<str>>(&self, msg: S) -> io::Result<()> {
+	if self.mp.is_hidden() {
+	    println!("{}", msg.as_ref());
+	    Ok(())
+	} else {
+	    self.mp.println(msg)
+	}
     }
 
     fn finish(&self) {
         self.pb_parts.finish();
         self.pb_total.finish();
-        self.pb_current.finish();
+    }
+}
+
+impl UploadProgress {
+    fn wrap_read<R: Read>(&self, read: R) -> UploadProgressReadWrapper<R> {
+	UploadProgressReadWrapper {
+	    read,
+	    pb_total: self.pb_total.clone(),
+	    pb_current: self.pb_current.clone(),
+	}
+    }
+
+    fn finish(&self) {
+	self.pb_current.finish();
+	self.pb_parts.inc(1);
+	self.mp.remove(&self.pb_current);
+    }
+
+    fn reset(&self) {
+	self.pb_current.reset();
     }
 }
 
 impl<R: Read> Read for UploadProgressReadWrapper<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let bytes_read = self.inner.read(buf)?;
+        let bytes_read = self.read.read(buf)?;
         self.pb_current.inc(bytes_read as u64);
         self.pb_total.inc(bytes_read as u64);
         Ok(bytes_read)
@@ -129,7 +158,7 @@ fn do_upload_part(
     url: &str,
     offset: u64,
     size: u64,
-    pb: &Option<UploadProgress>,
+    pb: &UploadProgress,
 ) -> Result<String, Box<dyn std::error::Error>> {
     let mut f = File::open(path)?;
     f.seek(SeekFrom::Start(offset))?;
@@ -137,19 +166,11 @@ fn do_upload_part(
     let part_reader = f.take(size);
     let buf_reader = BufReader::new(part_reader);
 
-    let req = uploader.url(url).mimetype("video/mp4");
-
-    let req = if let Some(pb) = pb {
-        req.from_reader_sized(pb.start_part(buf_reader, size), size)
-    } else {
-        req.from_reader_sized(buf_reader, size)
-    };
+    let req = uploader.url(url)
+	.mimetype("video/mp4")
+	.from_reader_sized(pb.wrap_read(buf_reader), size);
 
     let res = req.upload()?;
-
-    if let Some(pb) = pb {
-        pb.finish_part();
-    }
 
     Ok(res.etag)
 }
@@ -160,13 +181,14 @@ fn upload_part(
     url: &str,
     offset: u64,
     size: u64,
-    pb: &Option<UploadProgress>,
+    pb: UploadProgress,
     retry: u64,
 ) -> Result<String, Box<dyn std::error::Error>> {
     let mut retry_count = 0;
     loop {
-        let result = do_upload_part(uploader, path, url, offset, size, pb);
+        let result = do_upload_part(uploader, path, url, offset, size, &pb);
         if result.is_ok() {
+	    pb.finish();
             return result;
         }
 
@@ -174,7 +196,7 @@ fn upload_part(
         if retry_count >= retry {
             return result;
         }
-        pb.as_ref().map(|v| v.rollback_part());
+	pb.reset();
     }
 }
 
@@ -182,7 +204,7 @@ fn do_upload(
     uuid: &str,
     path: &Path,
     part_size: u64,
-    progress: bool,
+    no_progress: bool,
     retry: u64,
     hash: Option<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
@@ -195,35 +217,39 @@ fn do_upload(
 
     let parts = upload_start.urls.len() as u64;
 
-    let pb: Option<UploadProgress> = if progress {
-        let pb = UploadProgress::new(parts, f_size);
-        Some(pb)
-    } else {
-        None
-    };
-
-    let mut etags: Vec<String> = Vec::with_capacity(parts as usize);
+    let mp = UploadMultiProgress::new(parts, f_size);
+    if no_progress {
+	mp.hide();
+    }
 
     let uploader = s3::Uploader::new()?;
 
-    for (i, url) in upload_start.urls.iter().enumerate() {
+    let etags = upload_start.urls.par_iter().enumerate().map(|(i, url)| {
         let offset = (i as u64) * part_size;
         let size = min(part_size, f_size - offset);
-        let etag = upload_part(&uploader, path, url, offset, size, &pb, retry)?;
-        if !progress {
-            println!("Part uploaded {}/{}", i + 1, parts);
-        }
-        etags.push(etag);
-    }
+	let pb = mp.new_part(i, part_size);
+        let etag = upload_part(&uploader, path, url, offset, size, pb, retry)
+            .expect(format!("Failed to uploading part {}", i+1).as_str());
+        mp.println(format!("Part {} uploaded", i + 1)).unwrap();
+        etag
+    }).collect();
 
     api::video::upload_finish(uuid, upload_start.upload_id, etags, hash)?;
-    pb.map(|v| v.finish());
+    mp.finish();
 
     Ok(())
 }
 
 pub(crate) fn main(args: Args) {
     std::println!("Uploading video file {path}", path = args.path.display());
+
+    args.thread_count.map(|tc| {
+        println!("Setting thread count to {tc}");
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(tc)
+            .build_global()
+            .expect("Failed to set thread count")
+    });
 
     let hash = args
         .password
@@ -233,7 +259,7 @@ pub(crate) fn main(args: Args) {
         &args.uuid,
         &args.path,
         args.part_size,
-        args.progress,
+        args.no_progress,
         args.retry_part,
         hash,
     )
